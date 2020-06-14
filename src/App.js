d43f1955929f5068d5f8198d7addc87dd2aeaf41
Fiaz Ahmed <fayyazahmada@gmail.com>
2020-06-14 23:12:51 +0300

    multiple files updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Parent from './Parent';
 import CounterContext from './CounterContext';
 
 function App() {
-  let countState = useState(1) //[count, setCount]
+  //let countState = useState(1) //[count, setCount]
+  let [count, setCount] = useState(1);
 
   return (
     <CounterContext.Provider value= {countState}>
